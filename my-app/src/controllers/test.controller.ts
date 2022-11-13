9a83afa031fc9cb4f4fefed649dac543f3330c98
pallavi1505 <pallavishirodkar1505@gmail.com>
2022-11-13 03:27:05 +0000

    latest changes pushed by pallavi

---
+++ my-app/src/controllers/test.controller.ts
@@ -0,0 +1,18 @@
+// Uncomment these imports to begin using these cool features!
+
+// import {inject} from '@loopback/core';
+import {inject} from '@loopback/core';
+import {
+  Request,
+  RestBindings,
+  get,
+  response,
+  ResponseObject,
+} from '@loopback/rest';
+
+
+
+
+export class TestController {
+  constructor() {}
+}
